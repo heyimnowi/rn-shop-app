847c0414c1eea8047c0b69f1e0fdcc71a295dd7d
heyimnowi <lopeznoeliab@gmail.com>
2021-05-16 22:13:17 -0300

    Add | product item styles

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,33 +1,61 @@
-import React from 'react';
+import React from "react";
 import {
   ScrollView,
   View,
   Text,
   Image,
   Button,
-  StyleSheet
-} from 'react-native';
-import { useSelector } from 'react-redux';
+  StyleSheet,
+} from "react-native";
+import Colors from "../../constants/Colors";
+import { useSelector } from "react-redux";
 
-const ProductDetailScreen = props => {
-  const productId = props.navigation.getParam('productId');
-  const selectedProduct = useSelector(state =>
-    state.products.availableProducts.find(prod => prod.id === productId)
+const ProductDetailScreen = (props) => {
+  const productId = props.navigation.getParam("productId");
+  const selectedProduct = useSelector((state) =>
+    state.products.availableProducts.find((prod) => prod.id === productId)
   );
 
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title="Add to cart" onPress={() => {}} />
+      </View>
+      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
-ProductDetailScreen.navigationOptions = navData => {
+ProductDetailScreen.navigationOptions = (navData) => {
   return {
-    headerTitle: navData.navigation.getParam('productTitle')
+    headerTitle: navData.navigation.getParam("productTitle"),
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: "center"
+  },
+  price: {
+    fontSize: 20,
+    color: "#888",
+    textAlign: "center",
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 14,
+    color: "#666",
+    textAlign: "center",
+    marginVertical: 20,
+    marginHorizontal: 20
+  },
+});
 
 export default ProductDetailScreen;
